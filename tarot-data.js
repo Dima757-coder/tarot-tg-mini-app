370cde089a452cba7cc0ae7b4a8844fe7c43de64
Dima757-coder <mnqdouble@gmail.com>
2025-06-08 00:35:24 +0300

    tarot-data.js

--- tarot-data.js
+++ tarot-data.js
@@ -1,200 +1,111 @@
 const tarotCards = [
-  {
-    "name": "Маг",
-    "image_url_up": "https://iili.io/38S5Ab9.png ",
-    "image_url_down": "https://iili.io/38SU0rb.png ",
-    "meaning": {
-      "upright": "Власть, контроль, мастерство.",
-      "reversed": "Манипуляции, нехватка уверенности."
-    }
-  },
-  {
-    "name": "Жрица",
-    "image_url_up": "https://iili.io/38S5Vb1.png ",
-    "image_url_down": "https://iili.io/38SUGYx.png ",
-    "meaning": {
-      "upright": "Интуиция, подсознание, мудрость.",
-      "reversed": "Секретность, скрытые мотивы."
-    }
-  },
-  {
-    "name": "Императрица",
-    "image_url_up": "https://iili.io/38SRO9S.png ",
-    "image_url_down": "https://iili.io/38SSke2.png ",
-    "meaning": {
-      "upright": "Плодородие, забота, изобилие.",
-      "reversed": "Зависимость, эмоциональная нестабильность."
-    }
-  },
-  {
-    "name": "Император",
-    "image_url_up": "https://iili.io/38SRwS2.png ",
-    "image_url_down": "https://iili.io/38SSMxt.png ",
-    "meaning": {
-      "upright": "Власть, структура, лидерство.",
-      "reversed": "Деспотизм, чрезмерный контроль."
-    }
-  },
-  {
-    "name": "Иерофант",
-    "image_url_up": "https://iili.io/38S5HSp.png ",
-    "image_url_down": "https://iili.io/38SU91a.png ",
-    "meaning": {
-      "upright": "Традиции, духовность, учение.",
-      "reversed": "Несогласие, непослушание, отказ от норм."
-    }
-  },
-  {
-    "name": "Влюблённые",
-    "image_url_up": "https://iili.io/38S5uO7.png ",
-    "image_url_down": "https://iili.io/38SUcBe.png ",
-    "meaning": {
-      "upright": "Любовь, выбор, гармония.",
-      "reversed": "Конфликт, нелояльность, внутренний разлад."
-    }
-  },
-  {
-    "name": "Колесница",
-    "image_url_up": "https://iili.io/38SRa8N.png ",
-    "image_url_down": "https://iili.io/38SSEbI.png ",
-    "meaning": {
-      "upright": "Успех, победа, самоконтроль.",
-      "reversed": "Поражение, потеря направления, слабость."
-    }
-  },
-  {
-    "name": "Сила",
-    "image_url_up": "https://iili.io/38S5hWg.png ",
-    "image_url_down": "https://iili.io/38SUjQ1.png ",
-    "meaning": {
-      "upright": "Храбрость, выдержка, сострадание.",
-      "reversed": "Слабость, нетерпение, страх."
-    }
-  },
-  {
-    "name": "Отшельник",
-    "image_url_up": "https://iili.io/38SRsHB.png ",
-    "image_url_down": "https://iili.io/38SSL5Q.png ",
-    "meaning": {
-      "upright": "Мудрость, интроспекция, поиск правды.",
-      "reversed": "Изоляция, одиночество, уход от мира."
-    }
-  },
-  {
-    "name": "Колесо Фортуны",
-    "image_url_up": "https://iili.io/38SRvte.png ",
-    "image_url_down": "https://iili.io/38SSUX9.png ",
-    "meaning": {
-      "upright": "Удача, перемены, карма.",
-      "reversed": "Невезение, застой, циклы."
-    }
-  },
-  {
-    "name": "Правосудие",
-    "image_url_up": "https://iili.io/38S5TRS.png ",
-    "image_url_down": "https://iili.io/38SU3ep.png ",
-    "meaning": {
-      "upright": "Честность, закон, объективность.",
-      "reversed": "Неоправданность, коррупция, предвзятость."
-    }
-  },
-  {
-    "name": "Повешенный",
-    "image_url_up": "https://iili.io/38SRPSV.png ",
-    "image_url_down": "https://iili.io/38SSgse.png ",
-    "meaning": {
-      "upright": "Жертва, новая перспектива, принятие.",
-      "reversed": "Застой, упрямство, страх изменений."
-    }
-  },
-  {
-    "name": "Смерть",
-    "image_url_up": "https://iili.io/38SR0ut.png ",
-    "image_url_down": "https://iili.io/38SS1ON.png ",
-    "meaning": {
-      "upright": "Перемены, конец, преобразование.",
-      "reversed": "Сопротивление переменам, застой, страх."
-    }
-  },
-  {
-    "name": "Умеренность",
-    "image_url_up": "https://iili.io/38S5yXe.png ",
-    "image_url_down": "https://iili.io/38SUv3J.png ",
-    "meaning": {
-      "upright": "Баланс, гармония, целостность.",
-      "reversed": "Дисбаланс, нетерпение, деструктивное поведение."
-    }
-  },
-  {
-    "name": "Дьявол",
-    "image_url_up": "https://iili.io/38SRYap.png ",
-    "image_url_down": "https://iili.io/38SS0Rp.png ",
-    "meaning": {
-      "upright": "Искушение, привязанность, зависимость.",
-      "reversed": "Освобождение, пробуждение, осознание."
-    }
-  },
-  {
-    "name": "Башня",
-    "image_url_up": "https://iili.io/38S5pI9.png ",
-    "image_url_down": "https://iili.io/38SUSvR.png ",
-    "meaning": {
-      "upright": "Разрушение ложного, крах системы, внезапные перемены.",
-      "reversed": "Страх перед переменами, попытки сохранить старое."
-    }
-  },
-  {
-    "name": "Звезда",
-    "image_url_up": "https://iili.io/38S5XzF.png ",
-    "image_url_down": "https://iili.io/38SUVpV.png ",
-    "meaning": {
-      "upright": "Надежда, вдохновение, исцеление.",
-      "reversed": "Пессимизм, утраченная вера, разочарование."
-    }
-  },
-  {
-    "name": "Луна",
-    "image_url_up": "https://iili.io/38S55xe.png ",
-    "image_url_down": "https://iili.io/38SUE2j.png ",
-    "meaning": {
-      "upright": "Иллюзии, интуиция, подсознание.",
-      "reversed": "Обман, недоверие, скрытые страхи."
-    }
-  },
-  {
-    "name": "Солнце",
-    "image_url_up": "https://iili.io/38S5jsa.png ",
-    "image_url_down": "https://iili.io/38SUOEg.png ",
-    "meaning": {
-      "upright": "Счастье, успех, жизненная энергия.",
-      "reversed": "Пессимизм, неуверенность, временные трудности."
-    }
-  },
-  {
-    "name": "Суд",
-    "image_url_up": "https://iili.io/38S53Nt.png ",
-    "image_url_down": "https://iili.io/38SUHrJ.png ",
-    "meaning": {
-      "upright": "Пробуждение, восстановление, призвание.",
-      "reversed": "Вина, отрицание прошлого, самоосуждение."
-    }
-  },
-  {
-    "name": "Мир",
-    "image_url_up": "https://iili.io/38S7Jqb.png ",
-    "image_url_down": "https://iili.io/38SU4jI.png ",
-    "meaning": {
-      "upright": "Завершение, успех, гармония.",
-      "reversed": "Незавершённые дела, задержки, ограниченность."
-    }
-  },
-  {
-    "name": "Шут",
-    "image_url_up": "https://iili.io/38SRkN9.png ",
-    "image_url_down": "https://iili.io/38SSvbS.png ",
-    "meaning": {
-      "upright": "Начала, свобода, беззаботность.",
-      "reversed": "Безрассудство, беспечность, наивность."
-    }
-  }
+  {name: "Маг",
+        image_url_up: "https://iili.io/38S5Ab9.png", 
+        image_url_down: "https://iili.io/38SU0rb.png" 
+      },
+      {
+        name: "Жрица",
+        image_url_up: "https://iili.io/38S5Vb1.png", 
+        image_url_down: "https://iili.io/38SUGYx.png" 
+      },
+      {
+        name: "Императрица",
+        image_url_up: "https://iili.io/38SUTQv.png", 
+        image_url_down: "https://iili.io/38SUzUN.png" 
+      },
+      {
+        name: "Император",
+        image_url_up: "https://iili.io/38SUWcI.png", 
+        image_url_down: "https://iili.io/38SUjXG.png" 
+      },
+      {
+        name: "Священник",
+        image_url_up: "https://iili.io/38SUqJt.png", 
+        image_url_down: "https://iili.io/38SUaLr.png" 
+      },
+      {
+        name: "Влюблённые",
+        image_url_up: "https://iili.io/38SUeKg.png", 
+        image_url_down: "https://iili.io/38SUZnP.png" 
+      },
+      {
+        name: "Колесница",
+        image_url_up: "https://iili.io/38SUHsF.png", 
+        image_url_down: "https://iili.io/38SUXyR.png" 
+      },
+      {
+        name: "Сила",
+        image_url_up: "https://iili.io/38SUwEo.png", 
+        image_url_down: "https://iili.io/38SUlVn.png" 
+      },
+      {
+        name: "Отшельник",
+        image_url_up: "https://iili.io/38SUKkS.png", 
+        image_url_down: "https://iili.io/38SUgPh.png" 
+      },
+      {
+        name: "Колесо Фортуны",
+        image_url_up: "https://iili.io/38SUmdA.png", 
+        image_url_down: "https://iili.io/38SUtBj.png" 
+      },
+      {
+        name: "Справедливость",
+        image_url_up: "https://iili.io/38SUOQb.png", 
+        image_url_down: "https://iili.io/38SUvNp.png" 
+      },
+      {
+        name: "Повешенный",
+        image_url_up: "https://iili.io/38SU9uF.png", 
+        image_url_down: "https://iili.io/38SUFP2.png" 
+      },
+      {
+        name: "Смерть",
+        image_url_up: "https://iili.io/38SUQmS.png", 
+        image_url_down: "https://iili.io/38SURVx.png" 
+      },
+      {
+        name: "Умеренность",
+        image_url_up: "https://iili.io/38SUJsh.png", 
+        image_url_down: "https://iili.io/38SUPEM.png" 
+      },
+      {
+        name: "Дьявол",
+        image_url_up: "https://iili.io/38SUWcI.png", 
+        image_url_down: "https://iili.io/38SUjXG.png" 
+      },
+      {
+        name: "Башня",
+        image_url_up: "https://iili.io/38SUqJt.png", 
+        image_url_down: "https://iili.io/38SUaLr.png" 
+      },
+      {
+        name: "Звезда",
+        image_url_up: "https://iili.io/38SUeKg.png", 
+        image_url_down: "https://iili.io/38SUZnP.png" 
+      },
+      {
+        name: "Луна",
+        image_url_up: "https://iili.io/38SUHsF.png", 
+        image_url_down: "https://iili.io/38SUXyR.png" 
+      },
+      {
+        name: "Солнце",
+        image_url_up: "https://iili.io/38SUwEo.png", 
+        image_url_down: "https://iili.io/38SUlVn.png" 
+      },
+      {
+        name: "Суд",
+        image_url_up: "https://iili.io/38SUKkS.png", 
+        image_url_down: "https://iili.io/38SUgPh.png" 
+      },
+      {
+        name: "Мир",
+        image_url_up: "https://iili.io/38SUmdA.png", 
+        image_url_down: "https://iili.io/38SUtBj.png" 
+      },
+      {
+        name: "Шут",
+        image_url_up: "https://iili.io/38SUOQb.png", 
+        image_url_down: "https://iili.io/38SUvNp.png" 
+      }
 ]
